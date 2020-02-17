fdeed81eb0f784cb6405c2e5a61c856ef24e3011
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-17 14:39:08 -0500

    Proyecto inicial

---
+++ src/main/java/com/nb/springbootserviciozuulserver/SpringbootServicioZuulServerApplication.java
@@ -0,0 +1,14 @@
+package com.nb.springbootserviciozuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootServicioZuulServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootServicioZuulServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nb/springbootserviciozuulserver/SpringbootServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootserviciozuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
