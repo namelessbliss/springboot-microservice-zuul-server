247bfb78edc825d56a906ec2d18a2fd52631ecc4
namelessbliss <anonimo2911.2012@gmail.com>
2020-02-17 15:06:05 -0500

    Zuul filtro Http post, tiempo transcurrido

---
+++ src/main/java/com/nb/springbootserviciozuulserver/Filters/PostTiempoTranscurridoFilter.java
@@ -0,0 +1,48 @@
+package com.nb.springbootserviciozuulserver.Filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PostTiempoTranscurridoFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(PostTiempoTranscurridoFilter.class);
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+
+        log.info("Entrando a post");
+
+        Long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+        Long tiempoFinal = System.currentTimeMillis();
+        Long tiempoTranscurrido = tiempoFinal - tiempoInicio;
+
+        log.info(String.format("Tiempo transcurrido en segundos %s seg", tiempoTranscurrido.doubleValue() / 1000.00));
+        log.info(String.format("Tiempo transcurrido en milisegundos %s ms", tiempoTranscurrido));
+        return null;
+    }
+}
