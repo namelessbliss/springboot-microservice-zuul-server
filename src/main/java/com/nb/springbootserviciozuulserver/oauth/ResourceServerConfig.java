229286c5b106bd0b97b4cc1a28439e1bf63b6d5c
nb <anonimo2911.2012@gmail.com>
2020-03-05 19:10:26 -0500

    Configurando zuul como servidor de recursos

---
+++ src/main/java/com/nb/springbootserviciozuulserver/oauth/ResourceServerConfig.java
@@ -0,0 +1,52 @@
+package com.nb.springbootserviciozuulserver.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //permite que cualquier usuario se autentique
+        http.authorizeRequests().antMatchers("/api/security/oauth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/productos/listar", "/api/items/listar", "/api/usuarios/usuarios").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/productos/ver/{id}",
+                        "/api/items/ver/{id}/cantidad/{cantidad}",
+                        "/api/usuarios/usuarios/{id}")
+                .hasAnyRole("ADMIN", "USER")
+                /*
+                .antMatchers(HttpMethod.POST, "/api/productos/crear", "/api/items/crear", "/api/usuarios/usuarios").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/productos/editar/{id}", "/api/items/editar/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/productos/eliminar/{id}", "/api/items/eliminar/{id}", "/api/usuarios/usuarios/{id}").hasRole("ADMIN")
+                otra opcion, generalizando endpoints:*/
+                .antMatchers("/api/productos/**", "/api/items/**", "/api/usuarios/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStorage());
+    }
+
+    @Bean
+    public JwtTokenStore tokenStorage() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("codigo_secreto");
+        return tokenConverter;
+    }
+}
